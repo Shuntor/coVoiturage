4ac5176a04b1323f1707406b29b4e373e209ff03
unknown <quentin.hernandez0@gmail.com>
2015-05-06 15:34:40 +0200

    Ajout page gestionProfil.php non terminée

---
+++ include/gestionProfil.php
@@ -0,0 +1,63 @@
+ <!--<!DOCTYPE html> -->
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Site de covoiturage étudiant">
+    <meta name="VJMQ" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>CovoiturageEtudiant.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+      <div class="row marketing">
+        <div class="col-lg-12">
+          <div class="form-group annonce">
+          <h4>Gerer Mon Profil...</h4>
+          <ul >
+            <li class="list-unstyled">Nom : amorissa</li><br><br><!il faudrait un formulaire dans lequel la valeur serait mises et ou l'utilisateur pourrait modifier-->
+            <li class="list-unstyled">Prénom : nicoletta</li><br><br>
+            <li class="list-unstyled">Adresse e-mail : nicoletta.31000</li><br><br>
+            <li class="list-unstyled">Date de naissance : 20/06/2015</li><br><br>
+            <li class="list-unstyled">sexe : homme</li> <br><br>
+            <li class="list-unstyled">Adresse : 15 rue des roses</li>  <br>  <br>
+            <li class="list-unstyled">pays : ALBANIE </li>   <br>   <br>
+            <li class="list-unstyled"><a href="#">Changer Mon Mot de Passe...</a> </li><br><br>
+            <a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a>
+              
+          </ul>
+          </div>                 
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; Company 2014</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
