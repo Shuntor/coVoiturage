1d8b38215c5a4c87e1b315210cda73b64a34be1b
Shuntor <v.iungmann@gmail.com>
2015-05-06 17:41:10 +0200

    Ajout du gitignore

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
   <body>
 
-  <?php /*include 'connect.php';include 'fonctions.php'; session_start(); */?>
+  <?php include 'connect.php';/*include 'fonctions.php'; session_start(); */?>
 
 
    <div class="container">
