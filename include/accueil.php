226c6ced9ae177dc8071f53d2c6f70e39d5b8080
Shuntor <v.iungmann@gmail.com>
2015-05-06 15:16:09 +0200

    Modification index.php

---
+++ include/accueil.php
@@ -0,0 +1,102 @@
+<div class="jumbotron formRecherche">
+         <section>
+         <h1>RECHERCHE</h1>
+         <p class="lead">Rechercher votre trajet.</p>
+            <form>
+                
+                <div class="form-group col-lg-4">   
+                    <label for="texte">Départ :</label> 
+                    <input id= "text" type="text" class="form-control" placeholder="Ex: Toulouse,Paris,...">
+                </div> 
+                <div class="form-group col-lg-4">   
+                    <label for="texte">Arrivée :</label> 
+                    <input id= "text" type="text" class="form-control" placeholder="Ex: Toulouse,Paris,...">
+                </div> 
+                <div class="form-group col-lg-4">   
+                    <label for="texte">Date (jj/mm/aaaa) :</label> 
+                    <input id= "text" type="text" class="form-control" placeholder="Ex: 18/11/2015,...">
+                </div> 
+            </form>
+        </section>
+        <p><a class="btn btn-lg btn-success bouton rechercher" href="inscription.html" role="button">Rechercher!</a></p>
+          
+ </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <div class="form-group annonce">
+          <h4>TOULOUSE > LE MANS</h4>
+          <ul >
+            <li class="list-unstyled">Conducteur : nicoletta.31000</li>
+            <li class="list-unstyled">Point de Rendez-vous : Gare Matabiau, Toulouse 31000</li>
+            <li class="list-unstyled">Point d'Arrivée : Gare Routière, Le Mans, 72000</li>
+            <li class="list-unstyled">Date : 20/06/2015</li>
+            <li class="list-unstyled">Heure de Départ : 14h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Heure d'Arrrivée prévue : 22h15</li>
+            <li class="list-unstyled">Heure d'Arrrivée prévue : 22h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a></li>
+          </ul>
+          </div>  
+            
+          <div class="form-group annonce">
+          <h4>TOULOUSE > LE MANS</h4>
+          <ul >
+            <li class="list-unstyled">Conducteur : nicoletta.31000</li>
+            <li class="list-unstyled">Point de Rendez-vous : Gare Matabiau, Toulouse 31000</li>
+            <li class="list-unstyled">Point d'Arrivée : Gare Routière, Le Mans, 72000</li>
+            <li class="list-unstyled">Date : 20/06/2015</li>
+            <li class="list-unstyled">Heure de Départ : 14h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Heure d'Arrrivée prévue : 22h15</li>
+            <li class="list-unstyled">Heure d'Arrrivée prévue : 22h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a></li>
+          </ul>
+          </div>
+          
+            <div class="form-group annonce">
+          <h4>TOULOUSE > LE MANS</h4>
+          <ul >
+            <li class="list-unstyled">Conducteur : nicoletta.31000</li>
+            <li class="list-unstyled">Point de Rendez-vous : Gare Matabiau, Toulouse 31000</li>
+            <li class="list-unstyled">Point d'Arrivée : Gare Routière, Le Mans, 72000</li>
+            <li class="list-unstyled">Date : 20/06/2015</li>
+            <li class="list-unstyled">Heure de Départ : 14h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Heure d'Arrrivée prévue : 22h15</li>
+            <li class="list-unstyled">Heure d'Arrrivée prévue : 22h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a></li>
+          </ul>
+          </div>
+            
+        </div>
+
+        <div class="col-lg-6">
+          <div class="form-group annonce">
+          <h4>TOULOUSE > LE MANS</h4>
+          <ul >
+            <li class="list-unstyled">Conducteur : nicoletta.31000</li>
+            <li class="list-unstyled">Point de Rendez-vous : Gare Matabiau, Toulouse 31000</li>
+            <li class="list-unstyled">Point d'Arrivée : Gare Routière, Le Mans, 72000</li>
+            <li class="list-unstyled">Date : 20/06/2015</li>
+            <li class="list-unstyled">Heure de Départ : 14h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Heure d'Arrrivée prévue : 22h15</li>
+            <li class="list-unstyled">Heure d'Arrrivée prévue : 22h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a></li>
+          </ul>
+          </div>
+
+          <div class="form-group annonce">
+          <h4>TOULOUSE > LE MANS</h4>
+          <ul >
+            <li class="list-unstyled">Conducteur : nicoletta.31000</li>
+            <li class="list-unstyled">Point de Rendez-vous : Gare Matabiau, Toulouse 31000</li>
+            <li class="list-unstyled">Point d'Arrivée : Gare Routière, Le Mans, 72000</li>
+            <li class="list-unstyled">Date : 20/06/2015</li>
+            <li class="list-unstyled">Heure de Départ : 14h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Heure d'Arrrivée prévue : 22h15</li>
+            <li class="list-unstyled">Heure d'Arrrivée prévue : 22h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a></li>
+          </ul>
+          </div>
+
+          <div class="form-group annonce">
+          <h4>TOULOUSE > LE MANS</h4>
+          <ul >
+            <li class="list-unstyled">Conducteur : nicoletta.31000</li>
+            <li class="list-unstyled">Point de Rendez-vous : Gare Matabiau, Toulouse 31000</li>
+            <li class="list-unstyled">Point d'Arrivée : Gare Routière, Le Mans, 72000</li>
+            <li class="list-unstyled">Date : 20/06/2015</li>
+            <li class="list-unstyled">Heure de Départ : 14h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Heure d'Arrrivée prévue : 22h15</li>
+            <li class="list-unstyled">Heure d'Arrrivée prévue : 22h15 &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-lg btn-success bouton" href="inscription.html" role="button">Voir Détails...</a></li>
+          </ul>
+          </div>
+        </div>
+      </div>
