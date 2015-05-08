c7e5729648c8c161f0db33b5ec6da8fb47aba7c5
unknown <j.vaurigaud@gmail.com>
2015-05-08 17:56:13 +0200

    Mise à jour du formulaire d'inscription

--- include/inscription.php
+++ include/inscription.php
@@ -5,41 +5,55 @@
           <div class="form-group annonce">
           <h6>M'inscrire...</h6>
           <ul >
-            <form method="post" action="traitement.php" class="col-lg-8">
+            <form method="post" action="include/traitementInscription.php" class="col-lg-8">
         
                 <p><label for="nom" class="col-lg-3">Nom :</label>
                 <input type="text" name="nom" id="nom" /></p>
                 <br><br>
                 
                 <p><label for="prenom" class="col-lg-3">Prénom :</label>
-                <input type="password" name="prenom" id="prenom" /></p>
+                <input type="text" name="prenom" id="prenom" /></p>
                 <br><br>
                 
                 <p><label for="adresse" class="col-lg-3">Adresse :</label>
                 <input type="text" name="adresse" id="adresse" /></p>
                 <br><br>
                 
-                <p><label for="pays" class="col-lg-3">Pays :</label>
+                <p><label for="ville" class="col-lg-3">Ville :</label>
+                <input type="text" name="ville" id="ville" /></p>
+                <br><br>
+                
+                <p><label for="cp" class="col-lg-3">Code Postal :</label>
+                <input type="text" name="cp" id="cp" /></p>
+                <br><br>
+                
+                  <p><label for="pays" class="col-lg-3">Pays :</label>
                 <input type="text" name="pays" id="pays" /></p>
                 <br><br>
                 
-                <p><label for="dateNaissance" class="col-lg-3">Date de naissance :</label>
-                <input type="text" name="dateNaissance" id="datepicker" /></p>
+                <p><label for="dateNaissance" class="col-lg-3">Age :</label>
+                <input type="text" name="dateNaissance" id="dateNaissance" /></p>
                 <br><br>
                 
                 <p><label for="sexe" class="col-lg-3">Sexe :</label>
                 <input type="text" name="sexe" id="sexe" /></p>
                 <br><br>
                 
+                 <p><label for="telephone" class="col-lg-3">Téléphone :</label>
+                <input type="text" name="telephone" id="telephone" /></p>
+                <br><br>
+                
                 <p><label for="mail" class="col-lg-3">Adresse e-mail :</label>
                 <input type="text" name="mail" id="mail" /></p>
                 <br><br>
-                
+                                
                 <p><label for="inscriptionMdp" class="col-lg-3">Mot de Passe :</label>
-                <input type="text" name="inscriptionMdp" id="inscriptionMDP" /></p>
-                <p><label for="inscriptionconfMdp" class="col-lg-3">Confirmation du Mot de Passe :</label>
-                <input type="text" name="inscriptionconfMdp" id="inscriptionconfMdp" /></p>
+                <input type="password" name="inscriptionMdp" id="inscriptionMDP" /></p>
+                <p><label for="inscriptionConfMdp" class="col-lg-3">Confirmation du Mot de Passe :</label>
+                <input type="password" name="inscriptionConfMdp" id="inscriptionConfMdp" /></p>
                 <br><br>
+                
+                <input type="submit" value="Valider" >
                 
             </form>
             
