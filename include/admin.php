3cf4ad23fd1f2837e6a7bfbacedfd0e14dceecca
Iungmann <v.iungmann@gmail.com>
2015-05-29 00:07:25 +0200

    Suppression d'un utilisateur

--- include/admin.php
+++ include/admin.php
@@ -1,1 +1,43 @@
-zaedefzef
+<?php
+if (isset($_POST['supprimer'])) {	
+	$suppression="DELETE FROM CompteUtilisateur  WHERE idU='".$_POST['idU']."';";
+	$suppression=mysqli_query($conn, $suppression) or die('Erreur delete : '.mysqli_error($conn));
+}
+
+
+?>
+
+ <div class="row marketing">
+      <h2><center>Supprimer un utilisateur :</center></h2>
+       <div class="col-lg-12" >
+ 	<form method="post" action="index.php?p=admin" onSubmit="return verif(this)">
+ <?php
+ 
+    /* On récupère les derniers utilisateurs de 1 à 10 */
+    $reqUtilisateur="SELECT * FROM CompteUtilisateur  where idU!='".$_SESSION['idU']."'";
+	$reqUtilisateur=mysqli_query($conn, $reqUtilisateur) or die('Erreur select : '.mysqli_error($conn));
+
+		while ($utilisateur = mysqli_fetch_array($reqUtilisateur)){
+            ?>
+					
+    		<div class="form-group annonce mesutilisateurs col-lg-5 " style="margin-left:50px;margin-right:50;">
+          <h4><?php echo $utilisateur['prenomU']." > ".$utilisateur['nomU']; ?></h4>
+          <ul >
+            <li class="list-unstyled">ID : <?php echo $utilisateur['idU']; ?></li>
+            <li class="list-unstyled">Prenom: <?php echo $utilisateur['prenomU'] ?></li>
+            <li class="list-unstyled">Nom : <?php echo $utilisateur['nomU'] ?></li>
+            <li class="list-unstyled">Age : <?php echo $utilisateur['age']; ?></li>
+            <li class="list-unstyled">Genre : <?php echo $utilisateur['genre'] ?> </li>
+            <li class="list-unstyled">Mail : <?php echo $utilisateur['mail'] ?></li>
+            <li class="list-unstyled">Telephone : <?php echo $utilisateur['telephone'] ?></li>
+            <li class="list-unstyled">Pays : <?php echo $utilisateur['pays'] ?></li>
+            <input type="hidden"  name="idU"  value="<?php echo $utilisateur['idU'] ?>">		
+          </ul>
+            <center><input class="btn btn-danger" type='submit' value='Supprimer' name='supprimer' /></center>
+          </div> 
+		<?php
+        }
+    ?>
+	</form>
+ </div>
+ </div>
--- scriptBDD.sql
+++ scriptBDD.sql
@@ -28,6 +28,7 @@ annee int,
 idU varchar(150),
 constraint pk_voitures PRIMARY KEY(idV),
 constraint fk_voitures_compteUtilisateur FOREIGN KEY(idU) REFERENCES CompteUtilisateur(idU)
+ON DELETE CASCADE
 ) ;
 
 create table Trajets (
@@ -40,7 +41,7 @@ idVilleDepart int,
 idConducteur varchar(150),
 idVoiture int,
 constraint pk_trajet PRIMARY KEY(idT),
-constraint fk_trajet_compteUtilisateur FOREIGN KEY(idConducteur) REFERENCES CompteUtilisateur(idU),
+constraint fk_trajet_compteUtilisateur FOREIGN KEY(idConducteur) REFERENCES CompteUtilisateur(idU) ON DELETE CASCADE,
 constraint fk_trajet_ville_depart FOREIGN KEY(idVilleDepart) REFERENCES Villes(idVille),
 constraint fk_trajet_ville_destination FOREIGN KEY(idVilleDestination) REFERENCES Villes(idVille),
 constraint fk_trajet_id_voiture FOREIGN KEY(idVoiture) REFERENCES Voitures(idV)
@@ -53,8 +54,8 @@ idT int,
 texte varchar(500),
 note int,
 constraint pk_avis PRIMARY KEY(idDonneur, idReceveur, idT),
-constraint fk_idDonneur FOREIGN KEY(idDonneur) REFERENCES CompteUtilisateur(idU),
-constraint fk_idReceveur FOREIGN KEY(idReceveur) REFERENCES CompteUtilisateur(idU),
+constraint fk_idDonneur FOREIGN KEY(idDonneur) REFERENCES CompteUtilisateur(idU) ON DELETE CASCADE,
+constraint fk_idReceveur FOREIGN KEY(idReceveur) REFERENCES CompteUtilisateur(idU) ON DELETE CASCADE,
 constraint fk_idT FOREIGN KEY(idT) REFERENCES Trajets(idT)
 ) ;
 
@@ -72,6 +73,6 @@ nbPlace int,
 idU varchar(150),
 idT int,
 constraint fk_postuler_trajet FOREIGN KEY(idT) REFERENCES Trajets(idT),
-constraint fk_postuler_compteUtilisateur FOREIGN KEY(idU) REFERENCES CompteUtilisateur(idU),
+constraint fk_postuler_compteUtilisateur FOREIGN KEY(idU) REFERENCES CompteUtilisateur(idU) ON DELETE CASCADE,
 constraint pk_postuler PRIMARY KEY(idT,idU)
 ) ;
