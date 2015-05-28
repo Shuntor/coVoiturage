12b61348c54a90180c1387b30769c96b26798a59
hahahafr <ullrich.mailbox@gmail.com>
2015-05-29 00:51:09 +0200

    tab avis en gras

--- include/mesAvis.php
+++ include/mesAvis.php
@@ -17,12 +17,12 @@ INSERT INTO Avis(texte, idDonneur, idReceveur, idT, note) VALUES("SUPER !", 1, 1
         <div class="offset2 span8">
             <div class="tabbable">
                 <div class="col-lg-3"></div>
-                <center><ul class="nav nav-tabs nav-justified col-lg-6 barreAvis">
+                <center><strong><ul class="nav nav-tabs nav-justified col-lg-6 barreAvis">
                     <li class="active" class="bouton1"><a class="btnBarreAvis" href="#0" data-toggle="tab">Donner un avis</a></li>
-                    <li ><a href="#1" data-toggle="tab">Avis reçus</a></li>
+                    <li><a href="#1" data-toggle="tab">Avis reçus</a></li>
                     <li><a href="#2" data-toggle="tab">Avis donnés</a></li>
                  
-                </ul></center>
+                </ul></strong></center>
                 <div class="tab-content">
                     <div class="tab-pane active" id="0"><?php include("donnerAvis.php"); ?></div>
                     <div class="tab-pane " id="1"><?php include("avisRecus.php");  ?></div>
