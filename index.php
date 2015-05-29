aa0591e3e2aee113679b8656f6d2b7cb3c260a7c
hahahafr <ullrich.mailbox@gmail.com>
2015-05-29 11:35:27 +0200

    js mis dans un fichier

--- index.php
+++ index.php
@@ -20,76 +20,8 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
       <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>    
-  	<script> $.datepicker.regional['fr'] = {clearText: 'Effacer', clearStatus: '',
-      closeText: 'Fermer', closeStatus: 'Fermer sans modifier',
-      prevText: '&lt;Préc', prevStatus: 'Voir le mois précédent',
-      nextText: 'Suiv&gt;', nextStatus: 'Voir le mois suivant',
-      currentText: 'Courant', currentStatus: 'Voir le mois courant',
-      monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
-      'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-      monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
-      'Jul','Aoû','Sep','Oct','Nov','Déc'],
-      monthStatus: 'Voir un autre mois', yearStatus: 'Voir un autre année',
-      weekHeader: 'Sm', weekStatus: '',
-      dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
-      dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
-      dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
-      dayStatus: 'Utiliser DD comme premier jour de la semaine', dateStatus: 'Choisir le DD, MM d',
-      dateFormat: 'dd/mm/yy', firstDay: 1, 
-      initStatus: 'Choisir la date', isRTL: false};
-   $.datepicker.setDefaults($.datepicker.regional['fr']);
-  		$(function() {
-  			$("#datepicker").datepicker();
-  		});
-  	</script>
-      <script>
-       $(function(){     
-      var input = document.getElementById('ville');
-      var input2 = document.getElementById('ville2');         
-      var autocomplete = new google.maps.places.Autocomplete(input, {
-          types: ["(cities)"]
-      });                   
-      var autocomplete2 = new google.maps.places.Autocomplete(input2, {
-          types: ["(cities)"]
-      });          
-      
-      var infowindow = new google.maps.InfoWindow(); 
-      
-      $("input").focusin(function () {
-          $(document).keypress(function (e) {
-              if (e.which == 13) {
-                  infowindow.close();
-                  var firstResult = $(".pac-container .pac-item:first").text();
-                  
-                  var geocoder = new google.maps.Geocoder();
-                  geocoder.geocode({"address":firstResult }, function(results, status) {
-                      if (status == google.maps.GeocoderStatus.OK) {
-                          $("input").val(firstResult);
-                      }
-                  });
-              }
-          });
-      });
-      
-      $("input2").focusin(function () {
-          $(document).keypress(function (e) {
-              if (e.which == 13) {
-                  infowindow.close();
-                  var firstResult = $(".pac-container .pac-item:first").text();
-                  
-                  var geocoder = new google.maps.Geocoder();
-                  geocoder.geocode({"address":firstResult }, function(results, status) {
-                      if (status == google.maps.GeocoderStatus.OK) {
-                          $("input2").val(firstResult);
-                      }
-                  });
-              }
-          });
-      });
-  });
-
-
-  </script>
+      <script src="JS/datepicker.js"></script>
+      <script src="JS/autocomplete.js"></script>
       <title>CovoiturageEtudiant.com</title>
 
       <!-- Bootstrap core CSS -->
